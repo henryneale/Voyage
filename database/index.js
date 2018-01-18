7aa141a63360be3e8827a70016921f2e6ad4173c
toowan <mail@toowan.me>
2018-01-18 10:45:08 -0800

    (setup) Intial setup and Heroku deployment

--- database/index.js
+++ database/index.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
-//mongoose.connect('mongodb://localhost/tripcollab');
-mongoose.connect(process.env.MONGOLAB_PURPLE_URI);
+
+if (process.env.NODE_ENV === 'production') {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect('mongodb://localhost/tripcollab');
+}
+
 
 let restaurantSchema = mongoose.Schema({
   name: String,
